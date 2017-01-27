f4519d3f73ad588ce78cb268439cae08c8dc5573
Oded Dwek <oded.walla@gmail.com>
2017-01-27 13:32:04 +0200

    Added top navigation component

--- src/components/common/TopNavigation.js
+++ src/components/common/TopNavigation.js
@@ -1,24 +1,21 @@
 import React, { PropTypes } from 'react';
 import { Link, IndexLink } from 'react-router';
-import TopNavigation from './common/TopNavigation';
 
 // This is a class-based component because the current
 // version of hot reloading won't hot reload a stateless
 // component at the top-level.
-class App extends React.Component {
+class TopNavigation extends React.Component {
   render() {
     return (
       <div>
-        <TopNavigation />
-        <br/>
-        {this.props.children}
+        <IndexLink to="/">Home</IndexLink>
+        {' | '}
+        <Link to="/fuel-savings">Demo App</Link>
+        {' | '}
+        <Link to="/about">About</Link>
       </div>
     );
   }
 }
 
-App.propTypes = {
-  children: PropTypes.element
-};
-
-export default App;
+export default TopNavigation;
