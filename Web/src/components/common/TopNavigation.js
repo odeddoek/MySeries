78fcc270b7d15a7bb8d576fe02e0e9dc5e5b4956
Oded Dwek <oded.walla@gmail.com>
2017-02-10 23:33:45 +0200

    Bulma lied a little about their responsive hamburger menu, added a little code that makes it work.

--- Web/src/components/common/TopNavigation.js
+++ Web/src/components/common/TopNavigation.js
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link, IndexLink} from 'react-router';
 
 // This is a class-based component because the current
 // version of hot reloading won't hot reload a stateless
 // component at the top-level.
-class TopNavigation extends React.Component {
+class TopNavigation extends Component {
+
+  toggleMenu() {
+    let toggle = document.querySelector(".nav-toggle");
+    let menu = document.querySelector(".nav-menu");
+    toggle.classList.toggle("is-active");
+    menu.classList.toggle("is-active");
+  }
+
   render() {
     return (
       <nav className="nav">
         <div className="nav-left">
           <a href="#" className="nav-item logo-text">MySerias</a>
         </div>
-        <span className="nav-toggle">
+        <span id="nav-toggle" className="nav-toggle" onClick={this.toggleMenu.bind(this)}>
+          <span/>
           <span/>
           <span/>
           <span/>
         </span>
 
-        <div className="nav-right nav-menu">
+        <div id="nav-menu" className="nav-right nav-menu">
           <IndexLink to="/" className="nav-item">Home</IndexLink>
           <Link to="/login" className="nav-item">Login</Link>
           <Link to="/shows" className="nav-item">Shows list</Link>
