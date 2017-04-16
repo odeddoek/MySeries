4a67623a8acf93b418127cbda9111ca4e72b10c3
Oded Dwek <oded.walla@gmail.com>
2017-04-16 22:54:56 +0300

    App.js - removed useless footer import.

--- Web/src/components/App.js
+++ Web/src/components/App.js
@@ -1,6 +1,5 @@
 import React, {PropTypes} from 'react';
 import TopNavigation from './common/navigation/TopNavigation';
-import Footer from './common/Footer';
 
 // This is a class-based component because the current
 // version of hot reloading won't hot reload a stateless
