0f69398f8d41cc0b090e19b1d2df324f13b49018
Oded Dwek <oded.walla@gmail.com>
2017-05-05 00:02:07 +0300

    [Server] ShowRepository is now supporting of marking episode as watched, and also getting the list of watched episodes.

--- Server/src/bl/show-repository.ts
+++ Server/src/bl/show-repository.ts
@@ -10,7 +10,7 @@ class ShowRepository {
     public addUserTvShow(user: string, tvShowNumber: number) {
         return UserModel.findOne({ username: user }).then((user: IUser) => {
             if (this.isShowFollowed(user, tvShowNumber)) {
-                user.tvShows.push({ id: tvShowNumber, watchedEpisodes: [] as [number] });
+                user.tvShows.push({ id: tvShowNumber, watchedEpisodes: [] as [any] });
                 return user.save();
             }
         }, (err: any) => {
@@ -18,12 +18,23 @@ class ShowRepository {
         });
     }
 
+    public getWatchedEpisodes(user: string, tvShowId: number) {
+        return UserModel.findOne({ username: user, "tvShows.id": tvShowId }, { "tvShows.$": 1 }).then((user: IUser) => {
+            return user.tvShows[0].watchedEpisodes;
+        });
+    }
+
     public unfollowTvShow(user: string, tvShow: number) {
         return UserModel.update({ username: user }, { $pull: { tvShows: { id: tvShow } } });
     }
 
-    private isShowFollowed(user: IUser, tvShowNumber: number): boolean {
-        return user.tvShows.filter((show) => show.id === tvShowNumber).length === 0;
+    public markEpisodeAsWatched(user: string, tvShowId: number, season: number, episodeNumber: number) {
+        return UserModel.findOneAndUpdate({ username: user, "tvShows.id": tvShowId },
+            { $addToSet: { "tvShows.$.watchedEpisodes": { episodeNumber, season } } });
+    }
+
+    private isShowFollowed(user: IUser, tvShowId: number): boolean {
+        return user.tvShows.filter((show) => show.id === tvShowId).length === 0;
     }
 }
 
