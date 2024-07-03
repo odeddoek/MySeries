483795baeff20120fb746e75621decc601af61ac
Oded Dwek <30767512+odeddoek@users.noreply.github.com>
2024-07-03 17:48:09 +0300

    Update webpack.config.prod.js

--- Web/webpack.config.prod.js
+++ Web/webpack.config.prod.js
@@ -1,4 +1,4 @@
-// For info about this file refer to webpack and webpack-hot-middleware documentation
+// For info about this file refer to webpack and webpack-hot-middleare documentation
 // For info on how we're generating bundles with hashed filenames for cache busting: https://medium.com/@okonetchnikov/long-term-caching-of-static-assets-with-webpack-1ecb139adb95#.w99i89ns
 import webpack from 'webpack';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
